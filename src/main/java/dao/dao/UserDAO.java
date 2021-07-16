03c83b42435bc928b8ff74e4e32866cdc8d5b4ed
hamigakitanuki <kakizaki0213@gmail.com>
2021-07-16 11:00:43 +0900

    fix:UserDAO修正

--- src/main/java/dao/dao/UserDAO.java
+++ src/main/java/dao/dao/UserDAO.java
@@ -3,8 +3,8 @@ package dao.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import dao.bean.ChatRoomBean;
-import dao.bean.ChatRoomRecordBean;
+import dao.bean.UserBean;
+import dao.bean.UserRecordBean;
 
 public class UserDAO extends DAOBase {
 
@@ -16,12 +16,14 @@ public class UserDAO extends DAOBase {
 	@Override
 	public Object resultSetToBean(ResultSet rs) throws SQLException {
 
-		ChatRoomBean bean = new ChatRoomBean();
-		ChatRoomRecordBean record = new ChatRoomRecordBean();
+		UserBean bean = new UserBean();
+		UserRecordBean record = new UserRecordBean();
 		while (rs.next()) {
 			// 値セット
-			record.setChatRoomName(rs.getString("chat_room_name"));
+			record.setEmail(rs.getString("email"));
+			record.setName(rs.getString("name"));
 			record.setPassword(rs.getString("password"));
+			record.setType(rs.getInt("type"));
 
 			bean.addRecord(record);
 		}
