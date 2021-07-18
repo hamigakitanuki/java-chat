d5fa5f32426c4c5a91da6c66b6383df08b6d2da9
hamigakitanuki <kakizaki0213@gmail.com>
2021-07-19 00:37:12 +0900

    style:コードフォーマット

--- src/main/java/chat/Chat.java
+++ src/main/java/chat/Chat.java
@@ -27,42 +27,43 @@ import dao.dao.UserDAO;
  * Servlet implementation class Chat
  */
 @WebServlet("/Chat")
-@MultipartConfig(maxFileSize=1048576)
+@MultipartConfig(maxFileSize = 1048576)
 public class Chat extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	final File uploadDir = new File("upload");  // ファイル保存先
-
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Chat()  throws ServletException {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	final File uploadDir = new File("upload"); // ファイル保存先
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Chat() throws ServletException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// Query Stringは「param」キーでデータを取得する。
-//
-//    String param = request.getParameter("param");
-//
-//    if (param == null || param.isEmpty()) {
-//      param = "all";
-//    }
-//    // WebSessionに「TestSession」キーでデータを格納する。
-//
-//    System.out.println(param);
-//    request.getSession().setAttribute("chatRoomId", param);
-//
-//    // Web応答は「Session In OK」にする。
-//
-//    response.getWriter().append("Session In OK");
-		
+		// Query Stringは「param」キーでデータを取得する。
+		//
+		// String param = request.getParameter("param");
+		//
+		// if (param == null || param.isEmpty()) {
+		// param = "all";
+		// }
+		// // WebSessionに「TestSession」キーでデータを格納する。
+		//
+		// System.out.println(param);
+		// request.getSession().setAttribute("chatRoomId", param);
+		//
+		// // Web応答は「Session In OK」にする。
+		//
+		// response.getWriter().append("Session In OK");
+
 		HttpSession session = request.getSession();
 		int userId;
-		
+
 		/* userIdがない場合は作成し、セッションに格納 */
 		if (session.getAttribute("userId") == null) {
 			// 値をセット
@@ -74,55 +75,53 @@ public class Chat extends HttpServlet {
 			userId = dao.create(userRecordBeam);
 			session.setAttribute("userId", userId);
 		} else {
-			userId = (int) session.getAttribute("userId");	
+			userId = (int) session.getAttribute("userId");
 		}
-		
+
 		/* チャットルームメンバーテーブルから参加済みのチャットルームを取得 */
 		ChatRoomMemberBean chatRoomMemberBean;
 		ChatRoomMemberDAO chatRoomMemberDao = new ChatRoomMemberDAO();
 		chatRoomMemberDao.setWhere(String.format("user_id = %d", userId));
-	    chatRoomMemberBean = chatRoomMemberDao.getBean();
-	    
-	    /* chat_room_idを配列に変換 */
-	    List<String> chatRoomIds = new ArrayList<String>();
-	    for (ChatRoomMemberRecordBean chatRoomMemberRecordBean : chatRoomMemberBean.getRecordArray()) {
-	    	chatRoomIds.add(String.valueOf(chatRoomMemberRecordBean.getChatRoomId()));
-	    }
-	    
-	    ChatRoomBean chatRoomBean = null;
-	    if (chatRoomIds.size() > 0) { 
-	    	/* joinしてwhereInで検索し取得 */
-		    ChatRoomDAO chatRoomDao = new ChatRoomDAO();
-		    chatRoomDao.setWhere(String.format("id in (%s)", String.join(",", chatRoomIds)));
-		    chatRoomBean = chatRoomDao.getBean();
-	    }
-		
-	    session.setAttribute("chatRoomBean", chatRoomBean);
-	    getServletContext().getRequestDispatcher("/chat.jsp").forward(request, response);
-		    
+		chatRoomMemberBean = chatRoomMemberDao.getBean();
+
+		/* chat_room_idを配列に変換 */
+		List<String> chatRoomIds = new ArrayList<String>();
+		for (ChatRoomMemberRecordBean chatRoomMemberRecordBean : chatRoomMemberBean.getRecordArray()) {
+			chatRoomIds.add(String.valueOf(chatRoomMemberRecordBean.getChatRoomId()));
+		}
+
+		ChatRoomBean chatRoomBean = null;
+		if (chatRoomIds.size() > 0) {
+			/* joinしてwhereInで検索し取得 */
+			ChatRoomDAO chatRoomDao = new ChatRoomDAO();
+			chatRoomDao.setWhere(String.format("id in (%s)", String.join(",", chatRoomIds)));
+			chatRoomBean = chatRoomDao.getBean();
+		}
+
+		session.setAttribute("chatRoomBean", chatRoomBean);
+		getServletContext().getRequestDispatcher("/chat.jsp").forward(request, response);
 
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		doGet(request, response);
-		
-		Part fPart = request.getPart("file");
-	    String fName = (new StringBuilder(2)
-	      .append("").append(System.currentTimeMillis())
-	      .append("_").append(fPart.getSubmittedFileName()
-	    ).toString());
-	    String path = getServletContext().getRealPath("/upload");
-	    System.out.println(path+File.separator+fName);
-	    fPart.write(path+File.separator+fName);
 
-		//	ブロードキャスト処理
-		String chatRoomId = (String)request.getSession().getAttribute("chatRoomId");
-		BroadSocket.bloadCastSend(chatRoomId, "/upload/"+fName);
-	    response.getWriter().append("Send Ok");
+		Part fPart = request.getPart("file");
+		String fName = (new StringBuilder(2).append("").append(System.currentTimeMillis()).append("_")
+				.append(fPart.getSubmittedFileName()).toString());
+		String path = getServletContext().getRealPath("/upload");
+		System.out.println(path + File.separator + fName);
+		fPart.write(path + File.separator + fName);
+
+		// ブロードキャスト処理
+		String chatRoomId = (String) request.getSession().getAttribute("chatRoomId");
+		BroadSocket.bloadCastSend(chatRoomId, "/upload/" + fName);
+		response.getWriter().append("Send Ok");
 
 	}
 }
