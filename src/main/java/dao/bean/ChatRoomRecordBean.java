d951d5fbe3d88c483049671b76d3ea18a4a161d2
hamigakitanuki <kakizaki0213@gmail.com>
2021-07-18 22:17:16 +0900

    fix:チャットルームID取得処理追加

--- src/main/java/dao/bean/ChatRoomRecordBean.java
+++ src/main/java/dao/bean/ChatRoomRecordBean.java
@@ -6,16 +6,24 @@ import java.util.Date;
 public class ChatRoomRecordBean extends BaseRecordBean {
   private String chat_room_name;
   private String password;
+  private int id;
 
   public ChatRoomRecordBean() {
     super();
   }
-  
-  
+
   public String getValues() {
-	 Date d = new Date();
-     SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	 return String.format("values('%s', '%s', '%s')", chat_room_name, password, df.format(d));
+    Date d = new Date();
+    SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    return String.format("values('%s', '%s', '%s')", chat_room_name, password, df.format(d));
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getChatRoomName() {
